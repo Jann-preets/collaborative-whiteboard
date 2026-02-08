4af99776a47719a22cebd43f943e5627c86ea3cb
Janar Preethika <janar0629@gmail.com>
2026-02-08 12:21:27 +0530

    Initial commit: clean project structure

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  // This is where we will handle drawing data later
+  socket.on("send_drawing", (data) => {
+    socket.broadcast.emit("receive_drawing", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
+server.listen(5000, () => {
+  console.log("SERVER RUNNING ON PORT 5000");
+});
